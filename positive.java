7f9fbf4d17e5aa60394f20a52c17deb6fc75e473
SathishKumarMauriya <sathishkumar200298@gmail.com>
2018-01-20 19:01:13 +0530

    first commit

---
+++ positive.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main{
+public static void main (String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int a;
+    a=sc.nextInt();
+    if(a>0){
+        System.out.println("Positive");
+    }
+    else{
+        System.out.println("Negative");
+    }
+}
+
+}
