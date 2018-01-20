5ea0e22c661b4d34b4762751602d746abe5baf93
SathishKumarMauriya <34623077+SathishKumarMauriya@users.noreply.github.com>
2018-01-20 19:17:20 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main{
+public static void main (String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n;
+    n=sc.nextInt();
+    if(n%2==0){
+        System.out.println("Even");
+    }
+    else{
+        System.out.println("Odd");
+    }
+}
+
+}
