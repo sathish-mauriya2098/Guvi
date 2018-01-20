8d6e565bf9e15d7781353284cd068a89a42a0e2f
SathishKumarMauriya <34623077+SathishKumarMauriya@users.noreply.github.com>
2018-01-20 19:40:18 +0530

    Update Vowel.java

--- Vowel.java
+++ Vowel.java
@@ -4,7 +4,7 @@ public static void main (String[] args) {
     Scanner sc=new Scanner(System.in);
     char v;
     v=sc.next().charAt(0);
-    if(v=='a'||v=='e'||v=='i'||v=='o'||v=='o'){
+    if(v=='a'||v=='e'||v=='i'||v=='o'||v=='u'){
         System.out.println("Vowel");
     }
     else{
