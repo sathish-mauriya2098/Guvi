7619bb390dbfaafd1186175890b68f116e7f5fc2
SathishKumarMauriya <34623077+SathishKumarMauriya@users.noreply.github.com>
2018-01-28 12:10:22 +0530

    Reverse program

---
+++ Reverse.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main{
+public static void main (String[] args) {
+    Scanner sc=new Scanner(System.in);
+    StringBuffer s=new StringBuffer();
+    s.append(sc.nextLine());
+    System.out.println(s.reverse());
+}
+
+}
