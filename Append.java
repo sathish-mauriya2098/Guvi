5f669e14faf312781e69070432ce80a7ba13cb71
SathishKumarMauriya <34623077+SathishKumarMauriya@users.noreply.github.com>
2018-01-28 12:41:14 +0530

    Append Program

---
+++ Append.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main{
+    public static void main (String[] args) {
+        Scanner s=new Scanner(System.in);
+        String stl=s.nextLine();
+        StringBuffer sb=new StringBuffer(stl);
+        System.out.println(sb.append("."));
+        
+    }
+}
