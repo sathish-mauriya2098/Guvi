2de28f504628c9a3ec2bf5d95073ef15d5bfa609
SathishKumarMauriya <34623077+SathishKumarMauriya@users.noreply.github.com>
2018-01-28 12:34:31 +0530

    Reverse Number

---
+++ Revnumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main{
+public static void main (String[] args) {
+    int value,i=0;
+        Scanner s=new Scanner(System.in);
+        value=s.nextInt();
+        if(value>0)
+        {
+        i= i*10;
+          i=value%10;
+          value=value/10;
+        System.out.print(i);
+        }
+        else{
+            System.out.println("0");
+        }
+}
+
+}
