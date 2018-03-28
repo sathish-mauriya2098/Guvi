0b23acb8e89b298f4b3c12db62992f7ddd2fb5b6
SATHISH KUMAR MAURIYA M <34623077+sathish-mauriya2098@users.noreply.github.com>
2018-03-28 18:45:32 +0530

    Swap even by Odd character

--- swapeven.c
+++ swapeven.c
@@ -5,13 +5,12 @@ int main(){
      char str[20],tmp;
      int i;
      scanf("%s",str);
-     printf("\n\nOriginal String     : %s",str);
      for(i=0;i<strlen(str);i=i+2)
      {
      tmp = str[i];
      str[i] = str[i+1];
      str[i+1] = tmp;
      }
-     printf("\nAfter Swap String      : %s",str);
+     printf("\n%s",str);
      return 0;
 }
