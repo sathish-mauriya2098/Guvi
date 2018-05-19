424cc360000668c46f97c6b71ef72054002a4f8d
SATHISH KUMAR MAURIYA M <34623077+sathish-mauriya2098@users.noreply.github.com>
2018-05-19 13:32:29 +0530

    Print Repeat numbers

--- Repeat.java
+++ Repeat.java
@@ -2,21 +2,24 @@ import java.util.*;
 class Repeat{
 public static void main (String[] args) {
     Scanner sc=new Scanner(System.in);
-    String s=sc.nextLine();
-    int i,j,c=0;
-    char a[]=s.toCharArray();
-    Arrays.sort(a);
-    String s1=new String(a);
-    char b[]=s1.toCharArray();
-    for(i=0;i<s1.length();i++){
-        for(j=i+1;j<s1.length();j++){
-            if(b[i]==b[j]){
-                c++;
-                System.out.print(b[j]+" ");
-                break;
+    int i,j,k,c=0;
+    int n=sc.nextInt();
+    int s[]=new int[n];
+    for(k=0;k<n;k++){
+        s[k]=sc.nextInt();
+    }
+    Arrays.sort(s);
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(s[i]==s[j]){
+                c=1;
+                System.out.print(s[j]+" ");
             }
+            
         }
     }
+    if(c==0){
+        System.out.print("Unique");
+    }  
 }
-
 }
